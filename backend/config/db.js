6335ffdf98b461cf98eee8f14b822f6d61afe1b5
Suraj Kushwah <6266988978sksingh@gmail.com>
2025-09-19 17:02:10 +0530

    sk

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
    export const connectDB= async ()=>{
-     await mongoose.connect('mongodb+srv://6266988978sksingh:89593134@cluster0.v4hsx.mongodb.net/handmade-haat').then(()=>console.log("DB Connected"));
+     await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
 }
 
 
