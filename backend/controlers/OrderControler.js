5f22e6d5631a49aeaa829f441dbbd5bc29d1a105
Suraj Kushwah <6266988978sksingh@gmail.com>
2025-09-20 16:39:05 +0530

    sk

--- backend/controlers/OrderControler.js
+++ backend/controlers/OrderControler.js
@@ -12,7 +12,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req,res )=>{
 
-    const frontend_url='https://handmade-frontend.onrender.com'
+    const frontend_url=processs.env.FRONTEND_URL||"http://localhost:5174";
 
     try {
         const newOrder = new orderModel({
