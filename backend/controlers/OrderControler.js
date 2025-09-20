c7e1d18bb300ae82efb116519396383cf7aeaa2d
Suraj Kushwah <6266988978sksingh@gmail.com>
2025-09-20 15:28:06 +0530

    Update OrderControler.js

--- backend/controlers/OrderControler.js
+++ backend/controlers/OrderControler.js
@@ -12,7 +12,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req,res )=>{
 
-    const frontend_url='https://handmade-frontend.onrender.com/'
+    const frontend_url='https://handmade-frontend.onrender.com'
 
     try {
         const newOrder = new orderModel({
